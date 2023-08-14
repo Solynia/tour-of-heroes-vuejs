11dae70efdd4f7ed0b48c0bed57e6747005240e6
Sebastien Bouvier <sebastien@kyklo.co>
2023-08-14 23:11:41 +0700

    add new hero

--- src/components/HeroDetail.vue
+++ src/components/HeroDetail.vue
@@ -6,28 +6,31 @@ import { useRoute, useRouter } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
 
-const hero = ref<Hero | null>(null)
+const hero = ref<Hero>({ id: new Date(Date.now()).getTime(), name: '' })
 const input = ref<string>('')
-const formInvalid = computed(() => !input.value || (!!hero.value && input.value === hero.value.name))
+const formInvalid = computed(() => !input.value || input.value === hero.value.name)
 
 const redirectToList = () => router.push({ path: '/heroes/list' });
 const onSubmit = async () => {
   if (formInvalid.value) return;
+
   const response = await fetch(
-    `http://localhost:5174/heroes/${route.params.id}`,
+     `http://localhost:5174/heroes/${route.params.id ? route.params.id : ''}`,
     {
-      method: 'put',
+      method: route.params.id ? 'put' : 'post',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ...hero.value, name: input.value })
+      body: JSON.stringify({ id: hero.value.id, name: input.value })
     }
   );
-  if (response.status == 200) {
+  if ([200, 201].includes(response.status)) {
     redirectToList();
   }
 }
 
 
 watchEffect(async () => {
+  if (!route.params.id) return
+
   const response = await fetch(`http://localhost:5174/heroes/${route.params.id}`);
   if (response.status == 200) {
     hero.value = await response.json();
