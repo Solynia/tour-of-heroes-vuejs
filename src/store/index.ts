612a6e90279aa8f470fde4db869c6581943131f4
Sebastien Bouvier <sebastien.bouvier@outlook.com>
2025-04-11 23:35:16 +0700

    chore: integrate vuex

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+type State = {}
+
+const store = createStore<State>({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
+
+export default store
