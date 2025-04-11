77c8a60d0cb2510a39b7442df822b83c2a90c424
Sebastien Bouvier <sebastien.bouvier@outlook.com>
2025-04-12 02:58:46 +0700

    feat: use notification store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import type { Hero, HeroEntity } from '@/model/Hero'
+import type { Notification } from '@/model/Notification'
 import type { InjectionKey } from 'vue'
 import { useStore as baseUseStore, createStore, Store } from 'vuex'
 
@@ -6,7 +7,9 @@ type HeroesState = {
   heroes: HeroEntity[]
 }
 
-type NotificationState = {}
+type NotificationState = {
+  notification?: Notification
+}
 
 type State = {
   heroes: HeroesState
@@ -37,17 +40,25 @@ const store = createStore<State>({
     },
     fetchedHeroes: (state, heroes: HeroEntity[]) => {
       state.heroes.heroes = heroes
+    },
+    notificationAdded: (state, payload: Notification) => {
+      state.notifications.notification = payload
+    },
+    notificationDismissed: (state) => {
+      delete state.notifications.notification
     }
   },
   actions: {
-    createHero: async (_, { name }: Hero) => {
+    createHero: async ({ dispatch }, { name }: Hero) => {
+      dispatch('displayNotification', { message: `Saving ${name}` })
       await fetch(`http://localhost:5174/heroes`, {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id: new Date(Date.now()).getTime(), name })
       })
     },
-    updateHero: async ({ commit }, payload: HeroEntity) => {
+    updateHero: async ({ commit, dispatch }, payload: HeroEntity) => {
+      dispatch('displayNotification', { message: `Saving ${payload.name}` })
       const response = await fetch(`http://localhost:5174/heroes/${payload.id}`, {
         method: 'put',
         headers: { 'Content-Type': 'application/json' },
@@ -57,11 +68,21 @@ const store = createStore<State>({
         commit('updatedHero', payload)
       }
     },
-    fetchHeroes: async ({ commit }) => {
-      const response = await fetch('http://localhost:5174/heroes')
-      const heroes = (await response.json()) as { id: string; name: string }[]
-      const payload = heroes.map<HeroEntity>((h) => ({ id: +h.id, name: h.name }))
-      commit('fetchedHeroes', payload)
+    fetchHeroes: async ({ commit, dispatch }) => {
+      dispatch('displayNotification', { message: `Fetching heroes` })
+      try {
+        const response = await fetch('http://localhost:5174/heroes')
+        const heroes = (await response.json()) as { id: string; name: string }[]
+        const payload = heroes.map<HeroEntity>((h) => ({ id: +h.id, name: h.name }))
+        commit('fetchedHeroes', payload)
+        dispatch('displayNotification', { message: `Heroes loaded` })
+      } catch (error) {
+        dispatch('displayNotification', { message: `Error loading heroes` })
+      }
+    },
+    displayNotification: ({ commit }, payload: Notification) => {
+      setTimeout(() => commit('notificationDismissed'), 5000)
+      commit('notificationAdded', payload)
     }
   }
 })
